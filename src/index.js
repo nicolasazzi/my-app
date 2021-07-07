0edade56e5ef3f1520a93433d634143dd5ce4a30
nicolas azzi <nicolasazzi00@hotmail.com>
2021-07-07 17:13:58 +0300

    added search function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,72 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Product (product) {
+
+    const [showLarge, setShowLarge] = useState(false); 
+
+    return (
+        <div className='product' onClick={() => setShowLarge(!showLarge)}>
+            <p className='title'> {product?.title} </p>
+            { ! showLarge ? <img src={product?.images?.['0']?.thumbnail} /> : null }
+            { showLarge ? <img src={product?.images?.['0']?.large} /> : null }
+
+        </div>
+    );
+}
+
+function Results(result) {
+
+    if (result === {}) {
+        return (
+            <p> Testing </p>
+        );
+    }
+    else {
+
+        var products = result?.result?.data?.data?.products;
+
+        return (
+            <div> { products?.map((product) => Product(product) )} </div>
+        );
+    }
+}
+
+function App() {
+
+    const [result, setResult] = useState({})
+    const [keyword, setKeyword] = useState('')
+
+    function search(keyword, setResult) {
+
+        var headers = {
+            'Authorization': '446a6828200604377695aa034cf57e36',
+            'UserAddressId': '2378',
+            'StoreId': '1',
+        }
+    
+        var config = {
+            'headers': headers,
+        }
+
+        axios.get(`https://app.markitworld.com/api/v2/user/products/?keyword=${keyword}`, config).then((response) => setResult(response));
+    }
+
+    return (
+        <div>
+            <p> Search for a product: </p>
+            <input onChange={event => setKeyword(event.target.value)} type="text" />
+            <button onClick={() => search(keyword, setResult)}> Search </button>
+            <Results result={result}/>
+        </div>
+    ); 
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.getElementById('root')
+  );
